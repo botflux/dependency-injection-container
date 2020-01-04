09fea45d41705521089727f2f1b5bf5b61dcad4b
Victor MENDELE <v-mendele@logitud.fr>
2020-01-04 19:11:18 +0100

    :art: style: remove indentations in service not found error

--- src/errors/ServiceNotFoundError.ts
+++ src/errors/ServiceNotFoundError.ts
@@ -1,10 +1,10 @@
-    /**
-     * The service container has no service matching a given key.
-     */
-    export class ServiceNotFoundError extends Error {
-        constructor(key: string, ...args: any[]) {
-            super(...args)
-            this.message = `Service named ${key} doesn't exist`
-            this.name = 'ServiceNotFoundError'
-        }
+/**
+ * The service container has no service matching a given key.
+ */
+export class ServiceNotFoundError extends Error {
+    constructor(key: string, ...args: any[]) {
+        super(...args)
+        this.message = `Service named ${key} doesn't exist`
+        this.name = 'ServiceNotFoundError'
     }
+}
