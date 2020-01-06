e063570da04c499b1e0a6d578af780fb2f968ab4
Victor MENDELE <v-mendele@logitud.fr>
2020-01-06 10:14:49 +0100

    :star2: feat: add function to help the decorator creation

---
+++ src/helpers/IBoundInjectServiceConstructor.ts
@@ -0,0 +1,3 @@
+export interface IBoundInjectServiceConstructor {
+    (name: string): ParameterDecorator
+}
---
+++ src/helpers/IBoundInjectServiceProperty.ts
@@ -0,0 +1,3 @@
+export interface IBoundInjectServiceProperty {
+    (name: string): PropertyDecorator
+}
