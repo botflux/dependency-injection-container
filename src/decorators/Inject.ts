cc333063c21c6c62c54b1b05f06e898855dd6a24
Victor MENDELE <v-mendele@logitud.fr>
2020-01-07 15:58:58 +0100

    :hammer: fix: add ts ignore to inject decorator

--- src/decorators/Inject.ts
+++ src/decorators/Inject.ts
@@ -1,7 +1,9 @@
 export function Inject(serviceName: string) {
     return function (target: Object, propertyKey: string | symbol, parameterIndex: number) {
+        // @ts-ignore
         const injectionTokens = Reflect.getOwnMetadata('helloKey', target) || {}
         injectionTokens[parameterIndex] = serviceName
+        // @ts-ignore
         Reflect.defineMetadata('helloKey', injectionTokens, target)
     }
 }
