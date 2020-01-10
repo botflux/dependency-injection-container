9b16538f0d841e08b895d720bbfe142b8c4ca772
Victor MENDELE <v-mendele@logitud.fr>
2020-01-10 10:16:17 +0100

    :bug: test: remove unused container in inject test

--- tests/decorators/Inject.spec.ts
+++ tests/decorators/Inject.spec.ts
@@ -1,14 +1,9 @@
 import 'reflect-metadata'
-import {ServiceContainer} from '../..'
 import {Inject} from '../..'
 import {INJECT_TOKEN_METADATA} from '../..'
 
 describe('#InjectServiceConstructor', () => {
     it('adds metadata to the constructor parameter', () => {
-
-        const container = new ServiceContainer()
-            .addFactory('message', () => 'hello world')
-
         class MyService {
             constructor(@Inject('message') message: string) {
                 const tokens = Reflect.getOwnMetadata(INJECT_TOKEN_METADATA, MyService) || {}
