b3018c5cffa683da27b30c620735f16a1a342946
Victor MENDELE <v-mendele@logitud.fr>
2020-01-20 12:52:17 +0100

    :pencil: docs: add doc for create service container

--- src/factories/createServiceContainer.ts
+++ src/factories/createServiceContainer.ts
@@ -5,6 +5,16 @@ import {defaultOptions, ServiceContainerFactory} from './ServiceContainerFactory
 /**
  * Creates IServiceContainer instances. This function is an alias to service container factory's create method.
  *
+ * ```typescript
+ * const container = createServiceContainer()
+ * // ===
+ * const container = new ServiceContainerFactory().create()
+ *
+ * const container = createServiceContainer({ useReflection: true })
+ * // ===
+ * const container = new ServiceContainerFactory({ useReflection: true }).create()
+ * ```
+ *
  * @param config
  */
 export function createServiceContainer(config: IServiceContainerFactoryOptions = defaultOptions): IServiceContainer {
