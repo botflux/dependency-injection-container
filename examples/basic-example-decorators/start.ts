e7e8158e177527ffb7784b379074868fe1eae9e0
Victor MENDELE <v-mendele@logitud.fr>
2020-02-03 13:15:49 +0100

    :pencil: docs: change package path to npm registry in basic example decorators

--- examples/basic-example-decorators/start.ts
+++ examples/basic-example-decorators/start.ts
@@ -1,6 +1,6 @@
 // You should import one time reflect metadata before everything
 import 'reflect-metadata'
-import {IServiceContainerFactory, ServiceContainerFactory, Inject, IServiceContainer} from 'src'
+import {IServiceContainerFactory, ServiceContainerFactory, Inject, IServiceContainer} from '@botflx/dependency-injection-container'
 
 /**
  * We create a new factory. Produced IServiceContainer instances will be able to use reflection.
