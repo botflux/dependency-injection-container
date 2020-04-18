05ce70ff24d156ed8f09d344af0a51a565971166
Victor MENDELE <v-mendele@logitud.fr>
2020-04-18 18:57:07 +0200

    :herb: refactor: add more information in service already registered error

--- src/errors/ServiceAlreadyRegisteredError.ts
+++ src/errors/ServiceAlreadyRegisteredError.ts
@@ -1,7 +1,7 @@
 export class ServiceAlreadyRegisteredError extends Error {
     constructor(key: string, ...args: any[]) {
         super(...args)
-        this.message = `Service ${key} is already registered`
+        this.message = `Service ${key} is already registered. If you want to add service multiple times under the same name, you should set allowServiceOverride in createServiceFactory options`
         this.name = 'ServiceAlreadyRegisteredError'
     }
 }
