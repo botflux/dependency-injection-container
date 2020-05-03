813ef25d1e9ca05d9fdd125ff74b196a36c5daea
Victor MENDELE <v-mendele@logitud.fr>
2020-05-03 21:18:47 +0200

    :pencil: docs: add comments for plain service loader

--- src/loaders/ServiceLoader.ts
+++ src/loaders/ServiceLoader.ts
@@ -3,6 +3,26 @@ import {IServiceContainer} from '../IServiceContainer'
 import {IServiceFactoryFunction} from '../IServiceFactoryFunction'
 import {Constructor, ImportService} from '../types'
 
+/**
+ * Load a collection of service in a service container implementation.
+ *
+ * ```typescript
+ * import 'reflect-metadata'
+ * import {createServiceContainer, ServiceContainer, Constructor, ImportService} from '@botflx/dependency-injection-container'
+ *
+ * // List of service constructors
+ * const serviceConstructors: ImportService<Constructor>[] = [...]
+ * const serviceFactories: ImportService<IServiceFactoryFunction>[] = [...]
+ *
+ * // Creates a service container
+ * // In fact you can use a ReflectServiceLoader with a non-reflection service container.
+ * const container = createServiceContainer()
+ * const plainServiceLoader = new ServiceLoader(serviceConstructors, serviceFactories)
+ *
+ * // Load services in service loader
+ * plainServiceLoader.load(container)
+ * ```
+ */
 export class ServiceLoader implements IServiceLoader {
     private readonly _serviceConstructors: ImportService<Constructor>[]
     private readonly _serviceFactories: ImportService<IServiceFactoryFunction>[]
