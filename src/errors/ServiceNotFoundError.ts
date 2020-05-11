0a59298e0040467bc858bc245ddfb35d0de5a495
Victor MENDELE <v-mendele@logitud.fr>
2020-05-11 02:35:34 +0200

    :herb: refactor: add suggestion in service not found error

--- src/errors/ServiceNotFoundError.ts
+++ src/errors/ServiceNotFoundError.ts
@@ -4,7 +4,7 @@
 export class ServiceNotFoundError extends Error {
     constructor(key: string, ...args: any[]) {
         super(...args)
-        this.message = `Service named ${key} doesn't exist`
+        this.message = `Service named ${key} doesn't exist. Have you registered a service with "${key}" as key ?`
         this.name = 'ServiceNotFoundError'
     }
 }
